7346bf99bc8cb8fddcad564115116c70f3b12fb1
jiyeonyooo <ji04230423@gmail.com>
2025-01-02 15:52:48 +0900

    Add FirstPractice folder

---
+++ FirstPractice/FirstPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstPractice
+//
+//  Created by 유지연 on 1/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
