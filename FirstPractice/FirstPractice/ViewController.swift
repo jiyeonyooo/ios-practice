3cb3b7f1cbbf871db1eb66f2e39420bf22eb2e69
jiyeonyooo <ji04230423@gmail.com>
2025-01-04 17:35:32 +0900

    feat: pratice button pressed action

--- FirstPractice/FirstPractice/ViewController.swift
+++ FirstPractice/FirstPractice/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainLabel: UILabel! //변수
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        mainLabel.text = "안녕하세요"
+        mainLabel.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        mainLabel.textAlignment = NSTextAlignment.right
+    }
+    
 
 }
 
