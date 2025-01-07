0a627e3c5e4feedab9c88b1842d931e490a5fbb4
jiyeonyooo <ji04230423@gmail.com>
2025-01-07 23:38:46 +0900

    feat: check the action of outlet and outaction

--- FirstPractice/FirstPractice/ViewController.swift
+++ FirstPractice/FirstPractice/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var mainLabel: UILabel! //변수
+    @IBOutlet weak var myButton: UIButton!
     
-
+    //앱 화면에 들어오면 처음 실행시키는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -22,6 +23,9 @@ class ViewController: UIViewController {
         mainLabel.text = "안녕하세요"
         mainLabel.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         mainLabel.textAlignment = NSTextAlignment.right
+        
+        myButton.backgroundColor = UIColor.yellow
+        myButton.setTitleColor(UIColor.black, for: UIControl.State.normal)
     }
     
 
