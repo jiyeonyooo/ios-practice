b1391b2dcec87ae9d0e5c18175aeb7d152aad14e
jiyeonyooo <ji04230423@gmail.com>
2025-01-13 22:47:30 +0900

    feat: add roll button making dice result random

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstImgView: UIImageView!
+    @IBOutlet weak var secondImgView: UIImageView!
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func rollBtnTapped(_ sender: UIButton) {
+        
+        firstImgView.image = diceArray.randomElement()
+        secondImgView.image = diceArray.randomElement()
+    }
+    
 
 
 }
