36733325289fd69a7e33955e24077e1d61dd89cd
jiyeonyooo <ji04230423@gmail.com>
2025-01-20 14:24:28 +0900

    feat: implement function to make rps game logic

--- RPSGame/RPSGame/ViewController.swift
+++ RPSGame/RPSGame/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var comImgView: UIImageView!
+    @IBOutlet weak var myImgView: UIImageView!
+    
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        comImgView.image = #imageLiteral(resourceName: "rock")
+        myImgView.image = UIImage(named:"ready.png")
+        comChoiceLabel.text = "준비"
+        myChoiceLabel.text = "준비"
+        
     }
+    
+    
+    @IBAction func rpsBtnTapped(_ sender: UIButton) {
+    }
+    
 
-
+    @IBAction func selectBtnTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetBtnTapped(_ sender: UIButton) {
+    }
+    
 }
 
